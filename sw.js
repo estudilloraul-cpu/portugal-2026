86b970e1324dc912c2b6307eae461fbf7e0941da
estudilloraul-cpu <estudilloraul@gmail.com>
2026-07-17 00:43:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-
-const CACHE='explorer-family-portugal-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/trip.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{
-    const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;
-  }).catch(()=>caches.match('./index.html'))));
-});
+const CACHE='explorer-family-v3-hito1';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html')))));
