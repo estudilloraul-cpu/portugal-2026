d04f0aeedb6416c09ccc457f683b56857f487673
estudilloraul-cpu <estudilloraul@gmail.com>
2026-07-27 06:37:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,7 @@
-const CACHE='bitacora-v6-1-safari-fix';
-const ASSETS=["./", "./index.html", "./styles-v61.css?v=61", "./app-v61.js?v=61", "./data-v61.js?v=61", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener('install',function(){self.skipWaiting();});
+self.addEventListener('activate',function(event){
+  event.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(key){return caches.delete(key);}));
+  }).then(function(){return self.registration.unregister();}));
 });
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch',event=>{
-  const request=event.request;
-  const url=new URL(request.url);
-  const critical=request.mode==='navigate'||/\.(?:js|css)$/.test(url.pathname);
-  if(critical){
-    event.respondWith(
-      fetch(request,{cache:'no-store'})
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put(request,copy));
-          return response;
-        })
-        .catch(()=>caches.match(request).then(hit=>hit||caches.match('./index.html')))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(request).then(hit=>hit||fetch(request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(request,copy));
-      return response;
-    }))
-  );
-});
+self.addEventListener('fetch',function(){});
